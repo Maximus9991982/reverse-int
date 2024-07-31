edbb2f7082ac65821a7245498aec0cec62513410
Maximus9991982 <maximus999@tut.by>
2024-07-31 19:35:09 +0300

    init: Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
+
+    n = String(Math.abs(n));
+  
+   
+  
+    let numberTransformed = '';
+  
+    for (let i = n.length-1; i >=0; i--) {
+  
+        numberTransformed = numberTransformed + n[i];
+  
+    }
+  
+    n = Number(numberTransformed);
+  
+   
+  
+    return n;
   
 }
+
